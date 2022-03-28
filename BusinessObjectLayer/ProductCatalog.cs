e2c6df78ef88e752986e60049e892315164d7c99
subinbab <subinbabu4127@gmail.com>
2022-03-28 19:16:17 +0530

    Add project files.

---
+++ BusinessObjectLayer/ProductCatalog.cs
@@ -0,0 +1,93 @@
+﻿using DomainLayer;
+using log4net;
+using Repository;
+using System;
+using System.Collections.Generic;
+
+namespace BusinessObjectLayer
+{
+    public class ProductCatalog : IProductCatalog
+    {
+        private readonly ILog _log;
+        ProductDbContext _context;
+        IRepositoryOperations<Product> _repo;
+        public ProductCatalog(ProductDbContext context )
+        {
+            _log = LogManager.GetLogger(typeof(ProductCatalog));
+            _context = context;
+            _repo = new RepositoryOperations<Product>(_context); 
+        }
+        public void AddProduct(Product entity)
+        {
+            try
+            {
+                _repo.Add(entity);
+                _repo.Save();
+            }
+            catch(Exception ex)
+            {
+                _log.Error(ex);
+                throw ex;
+            }
+            
+        }
+
+        public void DeleteProduct(Product entity)
+        {
+            try
+            {
+                _repo.Delete(entity);
+                _repo.Save();
+            }
+            catch (Exception ex)
+            {
+                _log.Error(ex);
+                throw ex;
+            }
+            
+        }
+
+        public void EditProduct(Product entity)
+        {
+            try
+            {
+                _repo.Update(entity);
+                _repo.Save();
+            }
+            catch( Exception ex)
+            {
+                _log.Error(ex);
+                throw ex;
+            }
+            
+        }
+
+        public Product GetById(int id)
+        {
+            try
+            {
+                return _repo.GetById(id);
+            }
+            catch(Exception ex)
+            {
+                _log.Error(ex);
+                throw ex;
+            }
+            
+        }
+
+        public IEnumerable<Product> GetProduct()
+        {
+            try
+            {
+                return _repo.Get();
+            }
+            catch(Exception ex)
+            {
+                _log.Error(ex);
+                throw ex;
+            }
+            
+        }
+    }
+}
---
+++ Repository/RepositoryOperations.cs
@@ -0,0 +1,112 @@
+﻿using DomainLayer;
+using log4net;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Repository
+{
+    public class RepositoryOperations<T> : IRepositoryOperations<T> where T:class
+    {
+        private readonly ILog _log;
+        DbContext Context;
+        readonly DbSet<T> dbSet;
+        IEnumerable<T> entities;
+        T entity;
+       
+        public RepositoryOperations(DbContext product)
+        {
+            _log = LogManager.GetLogger(typeof(RepositoryOperations<T>));
+            Context = product;
+            dbSet = Context.Set<T>();
+           
+        }
+        public void Add(T entity)
+        {
+            try
+            {
+                dbSet.Add(entity);
+            }
+            catch(Exception ex)
+            {
+                _log.Error(ex);
+                throw ex;
+            }
+            
+        }
+
+        public void Delete(T entity)
+        {
+            try
+            {
+                dbSet.Remove(entity);
+            }
+            catch (Exception ex)
+            {
+                _log.Error(ex);
+                throw ex;
+            }
+            
+        }
+
+        public IEnumerable<T> Get()
+        {
+            try
+            {
+                entities = dbSet.ToList();
+            }
+            catch(Exception ex)
+            {
+                _log.Error(ex);
+                throw ex;
+            }
+            return entities;
+        }
+
+        public T GetById(int Id)
+        {
+            try
+            {
+                entity = dbSet.Find(Id);
+            }
+            catch(Exception ex)
+            {
+                _log.Error(ex);
+                throw ex;
+            }
+
+            return entity;
+        }
+
+        public void Save()
+        {
+            try
+            {
+                Context.SaveChanges();
+            }
+            catch(Exception ex)
+            {
+                _log.Error(ex);
+                throw ex;
+            }
+            
+        }
+
+        public void Update(T entity)
+        {
+            try
+            {
+                dbSet.Update(entity);
+            }
+            catch(Exception ex)
+            {
+                _log.Error(ex);
+                throw ex;
+            }
+            
+        }
+    }
+}
